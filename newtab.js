6a2863644870d010146f6c69a1f1f468989e05ce
UBU11 <uvaisubu11@gmail.com>
2025-07-24 18:03:26 +0530

    comic theme update

--- newtab.js
+++ newtab.js
@@ -1,5 +1,4 @@
 function formatTime(time) {
-
   return time < 10 ? `0${time}` : time;
 }
 
@@ -8,7 +7,6 @@ function startCountdown() {
   const hoursEl = document.getElementById("hours");
   const minutesEl = document.getElementById("minutes");
   const secondsEl = document.getElementById("seconds");
-
 
   const currentYear = new Date().getFullYear();
   const endDate = new Date(`December 31, ${currentYear} 23:59:59`);
@@ -19,12 +17,13 @@ function startCountdown() {
 
     if (timeLeft < 0) {
       clearInterval(interval);
-      document.getElementById("countdown").style.display = "none";
-      document.querySelector(".title").innerText = "A NEW YEAR HAS BEGUN!";
-      document.querySelector(".quote").innerText = "To a heroic new year!";
+      daysEl.innerText = "0";
+      hoursEl.innerText = "00";
+      minutesEl.innerText = "00";
+      secondsEl.innerText = "00";
+      document.querySelector(".title").innerText = "Happy New Year!";
       return;
     }
-
 
     const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
@@ -33,13 +32,11 @@ function startCountdown() {
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-
     daysEl.innerText = days;
     hoursEl.innerText = formatTime(hours);
     minutesEl.innerText = formatTime(minutes);
     secondsEl.innerText = formatTime(seconds);
   }, 1000);
 }
-
 
 startCountdown();
